37f46a5d65e95c8950fca9026acd52fd0ca2c9a3
Raja R Harinath <harinath@src.gnome.org>
1998-06-11 06:45:14 +0000

    Improve test harness. * Makefile.am (INCLUDES): Add GTK_CFLAGS. (dummy.c): Hack to force `automake' to define $COMPILE and $LINK. (TESTS_ENVIRONMENT): Pass $COMPILE and $LINK in environment to runtest.sh.
    * runtest.sh: Reorganized.  Makes better use of automake/libtool
    machinery to build and execute the tests.  Should work for
    builddir != srcdir.
    
    NOTE: Of course, all this is pretty useless right now.  All the
    testcases need to be updated.
    
    svn path=/trunk/; revision=1012

--- test-suite/runtest.sh
+++ test-suite/runtest.sh
@@ -1,44 +1,64 @@
 #!/bin/sh
-CC=cc
+
+test -z "$srcdir" && srcdir=`dirname $0` && test -z "$srcdir" && srcdir=.
+: ${COMPILE:="gcc -g -O2 -I.. -I$srcdir/.. `gtk-config --cflags` -c"}
+: ${LINK:="gcc -g -O2 $LDFLAGS"}
+: ${LDADD:="../libgnomeui/.libs/libgnomeui.a ../libgnome/.libs/libgnome.a `gtk-config --libs`"}
+
 exec 3>./results.summary
-if [ -z "$@" ]; then
-	TESTS=`find tests -name '*.c'`
+if test -z "$*"; then
+	TESTS="`find $srcdir/tests -name '*.c'`"
 else
 	TESTS="$@"
 fi
+
+mkdir tests || true > /dev/null 2>&1
+mkdir results || true > /dev/null 2>&1
+
+failed=no
+
 for I in $TESTS; do
-	TEST=`basename $I .c`
-	$CC tests/$TEST.c -L../../libgnomeui/.libs -lgnomeui -L../../libgnome/.libs \
-		-lgnome -I../.. -lgtk -lgdk -lglib -ldl -L/usr/X11R6/lib \
-		-lXext -lSM -lX11 -lm \
-		-Wl,-rpath,../libgnome/.libs \
-		-Wl,-rpath,../libgnomeui/.libs \
-		-Wl,-rpath,../../libgnome/.libs \
-		-Wl,-rpath,../../libgnomeui/.libs \
-		-g \
-		-o tests/$TEST > /tmp/.$$ 2>&1
-	T=$?
-	if [ $T != 0 ]; then
-		echo "CFAIL: $TEST" >&3
-		echo "CFAIL: $TEST"
-		mv /tmp/.$$ results/$TEST.out
-	else
-		rm -f /tmp/.$$
-		tests/$TEST > results/$TEST.out 2>&1
-		T=$?
-		echo "Exit code $T" >> results/$TEST.out
-		if [ $T != 0 ]; then
-			echo "FAIL: $TEST" >&3
-			echo "FAIL: $TEST"
-			rm -f core
-		else
-			if diff -q expected/$TEST.out results/$TEST.out; then
-				echo "PASS: $TEST" >&3
-				echo "PASS: $TEST"
-			else
-				echo "FAIL: $TEST" >&3
-				echo "FAIL: $TEST"
-			fi
-		fi
-	fi
+    TEST=`basename $I .c`
+    $COMPILE $srcdir/tests/$TEST.c -o tests/$TEST.o > /tmp/.$$ 2>&1
+    T=$?
+    if test $T != 0; then
+	echo "CFAIL: $TEST" >&3
+	echo "CFAIL: $TEST"
+	cp /tmp/.$$ results/$TEST.out && rm -f /tmp/.$$
+	failed=yes
+	continue
+    fi
+
+    $LINK tests/$TEST.o $LDADD $LIBS >> /tmp/.$$ 2>&1
+    T=$?
+    if test $T != 0; then
+	echo "CFAIL: $TEST" >&3
+	echo "CFAIL: $TEST"
+	cp /tmp/.$$ results/$TEST.out && rm -f /tmp/.$$
+	failed=yes
+	continue
+    fi
+
+    rm -f /tmp/.$$
+    tests/$TEST > results/$TEST.out 2>&1
+    T=$?
+    echo "Exit code $T" >> results/$TEST.out
+    if test $T != 0; then
+	echo "FAIL: $TEST" >&3
+	echo "FAIL: $TEST"
+	rm -f core
+	failed=yes
+	continue
+    fi
+
+    if diff -q $srcdir/expected/$TEST.out results/$TEST.out; then
+	echo "PASS: $TEST" >&3
+	echo "PASS: $TEST"
+    else
+	echo "FAIL: $TEST" >&3
+	echo "FAIL: $TEST"
+    fi
 done
+
+if test "$failed" = "yes"; then exit 1; fi
+exit 0
