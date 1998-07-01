0322934426276bb9010bd720a35808906c822798
Federico Mena Quintero <federico@nuclecu.unam.mx>
1998-07-01 02:28:27 +0000

    Here is the new canvas that uses GtkObjects for the canvas items. It
    1998-06-30  Federico Mena Quintero  <federico@nuclecu.unam.mx>
    
    	* gnome-canvas.c:
    	* gnome-canvas.h:
    	* gnome-canvas-rect-ellipse.c:
    	* gnome-canvas-rect-ellipse.h: Here is the new canvas that uses
    	GtkObjects for the canvas items.  It supports a hierarchical
    	organization of items, and arbitrary item sets as well.  This is
    	still a work in progress.  Please see the test-gnome demo for
    	information on how to use this beast.
    
    svn path=/trunk/; revision=1082

--- test-gnome/canvas.c
+++ test-gnome/canvas.c
@@ -3,79 +3,25 @@
 
 
 #ifdef GTK_HAVE_ACCEL_GROUP
-static void
-set_pixels_per_unit (GtkAdjustment *adj, GnomeCanvas *canvas)
-{
-	gnome_canvas_set_pixels_per_unit (canvas, adj->value);
-}
-
-typedef void (* ItemFunc) (GnomeCanvas *canvas, GnomeCanvasId cid, GdkEvent *event);
-
-static double drag_x, drag_y;
-
-static void
-start_drag (GnomeCanvas *canvas, GnomeCanvasId cid, GdkEvent *event)
-{
-	drag_x = event->motion.x;
-	drag_y = event->motion.y;
-}
-
-static void
-drag (GnomeCanvas *canvas, GnomeCanvasId cid, GdkEvent *event)
-{
-	double dx, dy;
-
-	if (!(event->motion.state & GDK_BUTTON1_MASK))
-		return;
-
-	dx = (event->motion.x - drag_x) / canvas->pixels_per_unit;
-	dy = (event->motion.y - drag_y) / canvas->pixels_per_unit;
-
-	gnome_canvas_move (canvas, cid, dx, dy);
-
-	drag_x = event->motion.x;
-	drag_y = event->motion.y;
-}
 
 static gint
-item_event (GnomeCanvas *canvas, GnomeCanvasId *item, GdkEvent *event, gpointer item_data, gpointer user_data)
+item_event (GnomeCanvasItem *item, GdkEvent *event, gpointer data)
 {
-	ItemFunc func;
+	char *color;
 
 	switch (event->type) {
 	case GDK_ENTER_NOTIFY:
-		gnome_canvas_configure (canvas, *item,
-					GNOME_CANVAS_FILL_COLOR, GNOME_CANVAS_COLOR_STRING, item_data,
-					GNOME_CANVAS_END);
+		color = gtk_object_get_data (GTK_OBJECT (item), "highlight");
+		gnome_canvas_item_set (item,
+				       "GnomeCanvasRE::fill_color", color,
+				       NULL);
 		break;
 
 	case GDK_LEAVE_NOTIFY:
-		gnome_canvas_configure (canvas, *item,
-					GNOME_CANVAS_FILL_COLOR, GNOME_CANVAS_COLOR_STRING, item_data,
-					GNOME_CANVAS_END);
-		break;
-
-	case GDK_BUTTON_PRESS:
-		gnome_canvas_configure (canvas, *item,
-					GNOME_CANVAS_OUTLINE_COLOR, GNOME_CANVAS_COLOR_STRING, "cyan",
-					GNOME_CANVAS_WIDTH_PIXELS, 5,
-					GNOME_CANVAS_END);
-		func = item_data;
-		if (func)
-			(* func) (canvas, *item, event);
-			
-		break;
-
-	case GDK_BUTTON_RELEASE:
-		gnome_canvas_configure (canvas, *item,
-					GNOME_CANVAS_OUTLINE_COLOR, GNOME_CANVAS_COLOR_NONE,
-					GNOME_CANVAS_END);
-		break;
-
-	case GDK_MOTION_NOTIFY:
-		func = item_data;
-		if (func)
-			(* func) (canvas, *item, event);
+		color = gtk_object_get_data (GTK_OBJECT (item), "normal");
+		gnome_canvas_item_set (item,
+				       "GnomeCanvasRE::fill_color", color,
+				       NULL);
 		break;
 
 	default:
@@ -89,86 +35,50 @@ void
 create_canvas (void)
 {
 	GtkWidget *app;
-	GtkWidget *hbox;
-	GtkWidget *vbox;
-	GtkWidget *w;
-	GtkWidget *frame;
 	GtkWidget *canvas;
-	GtkAdjustment *adj;
-	GnomeCanvasId cid;
+	GnomeCanvasGroup *root;
+	GnomeCanvasItem *item;
+	int x, y;
+	char buf[100];
+	char *color;
 
 	app = create_newwin (TRUE, "testGNOME", "Canvas");
-
-	hbox = gtk_hbox_new (FALSE, GNOME_PAD_SMALL);
-	gnome_app_set_contents (GNOME_APP (app), hbox);
-	gtk_widget_show (hbox);
 
 	canvas = gnome_canvas_new (gtk_widget_get_default_visual (), gtk_widget_get_default_colormap ());
 	gnome_canvas_set_size (GNOME_CANVAS (canvas), 500, 500);
 	gnome_canvas_set_pixels_per_unit (GNOME_CANVAS (canvas), 5.0);
 	gnome_canvas_set_scroll_region (GNOME_CANVAS (canvas), 0.0, 0.0, 100.0, 100.0);
-	gtk_signal_connect (GTK_OBJECT (canvas), "item_event",
-			    (GtkSignalFunc) item_event,
-			    NULL);
 
-	vbox = gtk_vbox_new (FALSE, GNOME_PAD_SMALL);
-	gtk_box_pack_start (GTK_BOX (hbox), vbox, FALSE, FALSE, 0);
-	gtk_widget_show (vbox);
+	/* Create items */
 
-	adj = GTK_ADJUSTMENT (gtk_adjustment_new (1.0, 0.1, 1000.0, 0.1, 10.0, 10.0));
-	gtk_signal_connect (GTK_OBJECT (adj), "value_changed",
-			    (GtkSignalFunc) set_pixels_per_unit,
-			    canvas);
+	root = GNOME_CANVAS_GROUP (gnome_canvas_root (GNOME_CANVAS (canvas)));
 
-	w = gtk_spin_button_new (adj, 0.5, 1);
-	gtk_widget_set_usize (w, 60, 0);
-	gtk_box_pack_start (GTK_BOX (vbox), w, FALSE, FALSE, 0);
-	gtk_widget_show (w);
+	for (y = 0; y < 100; y += 10)
+		for (x = 0; x < 100; x += 10) {
+			sprintf (buf, "#%02x%02x80", (x * 255) / 90, (y * 255) / 90);
+			color = g_strdup (buf);
+			item = gnome_canvas_item_new (GNOME_CANVAS (canvas), root, gnome_canvas_rect_get_type (),
+						      "GnomeCanvasRE::x1", (double) x,
+						      "GnomeCanvasRE::y1", (double) y,
+						      "GnomeCanvasRE::x2", (double) (x + 10),
+						      "GnomeCanvasRE::y2", (double) (y + 10),
+						      "GnomeCanvasRE::fill_color", color,
+						      "GnomeCanvasRE::outline_color", "black",
+						      "GnomeCanvasRE::width_pixels", 1,
+						      NULL);
+			gtk_object_set_data (GTK_OBJECT (item), "normal", color);
+			gtk_object_set_data (GTK_OBJECT (item), "highlight", "white");
+			gtk_signal_connect (GTK_OBJECT (item), "event",
+					    (GtkSignalFunc) item_event,
+					    NULL);
+		}
 
-	gnome_canvas_create_item (GNOME_CANVAS (canvas), "ellipse",
-				  20.0, 20.0, 80.0, 80.0,
-				  GNOME_CANVAS_FILL_COLOR, GNOME_CANVAS_COLOR_STRING, "red",
-				  GNOME_CANVAS_OUTLINE_COLOR, GNOME_CANVAS_COLOR_STRING, "black",
-				  GNOME_CANVAS_WIDTH_PIXELS, 10,
-				  GNOME_CANVAS_END);
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_ENTER_NOTIFY, "white");
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_LEAVE_NOTIFY, "red");
+	/* Done! */
 
-	cid = gnome_canvas_create_item (GNOME_CANVAS (canvas), "rectangle",
-					50.0, 35.0, 90.0, 50.0,
-					GNOME_CANVAS_FILL_COLOR, GNOME_CANVAS_COLOR_STRING, "green",
-					GNOME_CANVAS_END);
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_ENTER_NOTIFY, "white");
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_LEAVE_NOTIFY, "green");
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_BUTTON_PRESS, start_drag);
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_MOTION_NOTIFY, drag);
-
-	cid = gnome_canvas_create_item (GNOME_CANVAS (canvas), "rectangle",
-					20.0, 65.0, 50.0, 80.0,
-					GNOME_CANVAS_FILL_COLOR, GNOME_CANVAS_COLOR_STRING, "yellow",
-					GNOME_CANVAS_OUTLINE_COLOR, GNOME_CANVAS_COLOR_STRING, "blue",
-					GNOME_CANVAS_WIDTH_PIXELS, 5,
-					GNOME_CANVAS_END);
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_ENTER_NOTIFY, "white");
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_LEAVE_NOTIFY, "yellow");
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_BUTTON_PRESS, start_drag);
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_MOTION_NOTIFY, drag);
-
-	cid = gnome_canvas_create_tag (GNOME_CANVAS (canvas), GNOME_CANVAS_ALL);
-
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_BUTTON_PRESS, NULL);
-	gnome_canvas_bind (GNOME_CANVAS (canvas), cid, GDK_BUTTON_RELEASE, NULL);
-
-	frame = gtk_frame_new (NULL);
-	gtk_frame_set_shadow_type (GTK_FRAME (frame), GTK_SHADOW_IN);
-	gtk_box_pack_start (GTK_BOX (hbox), frame, TRUE, TRUE, 0);
-	gtk_widget_show (frame);
-
-	gtk_container_add (GTK_CONTAINER (frame), canvas);
+	gnome_app_set_contents (GNOME_APP (app), canvas);
 	gtk_widget_show (canvas);
 
 	gtk_widget_show (app);
 }
 
 #endif
-
