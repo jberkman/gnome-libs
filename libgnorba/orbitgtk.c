92fc3c807c071fd28660654eed65c85e56dba001
Elliot Lee <sopwith@src.gnome.org>
1998-08-17 03:49:37 +0000

    libgnorba: orbitgtk.c: name service launching "should" work. Move all .idl
    libgnorba:
    	orbitgtk.c: name service launching "should" work.
    Move all .idl files into $(top_srcdir)/idl directory.
    
    svn path=/trunk/; revision=1349

--- libgnorba/orbitgtk.c
+++ libgnorba/orbitgtk.c
@@ -1,4 +1,7 @@
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <limits.h>
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
 #include "gnorba.h"
@@ -6,6 +9,11 @@
 static void
 orb_handle_connection(GIOPConnection *cnx, gint source, GdkInputCondition cond)
 {
+
+  /* The best way to know about an fd exception is if select()/poll()
+     tells you about it, so we just relay that information on to ORBit
+     if possible */
+
   switch(cond) {
   case GDK_INPUT_EXCEPTION:
     giop_main_handle_connection_exception(cnx);
@@ -32,7 +40,7 @@ static void orb_remove_connection(GIOPConnection *cnx)
 static CORBA_ORB orb;
 
 CORBA_ORB
-gnorba_ORB_init(int *argc, char **argv, CORBA_ORBid orb_identifier,
+gnome_CORBA_ORB_init(int *argc, char **argv, CORBA_ORBid orb_identifier,
 		CORBA_Environment *ev)
 {
   CORBA_ORB retval;
@@ -44,27 +52,85 @@ gnorba_ORB_init(int *argc, char **argv, CORBA_ORBid orb_identifier,
   return retval;
 }
 
+/* This routine bootstraps CORBA connectivity for a GNOME desktop session... */
 CORBA_Object
-gnorba_get_name_service(void)
+gnome_get_name_service(void)
 {
   static CORBA_Object name_service = CORBA_OBJECT_NIL;
+  CORBA_Object retval;
+  char *ior;
+  GdkAtom propname, proptype;
+  CORBA_Environment ev;
 
-  if(!name_service) {
-    char *ior;
-    int pid;
-    CORBA_Environment ev;
+  g_return_val_if_fail(orb, CORBA_OBJECT_NIL);
 
-    CORBA_exception_init(&ev);
+  CORBA_exception_init(&ev);
 
-    /* Here: fork & get the ior from orbit-name-service stdout */
-    g_error("NYI");
+  propname = gdk_atom_intern("CORBA_NAME_SERVICE", FALSE);
+  proptype = gdk_atom_intern("XA_STRING", FALSE);
 
-    gdk_property_change(GDK_ROOT_PARENT(),
-			gdk_atom_intern("CORBA_NAME_SERVICE", FALSE),
-			gdk_atom_intern("XA_STRING", FALSE), 8,
-			GDK_PROP_MODE_REPLACE, ior, strlen(ior));
-    name_service = CORBA_ORB_string_to_object(orb, ior, &ev);
+  if(CORBA_Object_is_nil(name_service, &ev)) {
+    int iopipes[2];
+    GdkAtom return_proptype;
+    gint fmt, len;
+    char iorbuf[2048];
+
+    /* First, try and see if another application has started the name service
+       (and indicated its presence by setting a root window property */
+    if(gdk_property_get(GDK_ROOT_PARENT(), propname, proptype,
+			0, UINT_MAX, FALSE, &return_proptype,
+			&fmt, &len, (guchar **)&ior)) {
+      name_service = CORBA_ORB_string_to_object(orb, ior, &ev);
+      if(ev._major != CORBA_NO_EXCEPTION)
+	name_service = CORBA_OBJECT_NIL;
+      goto out;
+    }
+
+    /* Since we're pretty sure no name server is running, we start it ourself
+       and tell the (GNOME session) world about it */
+    /* fork & get the ior from orbit-name-service stdout */
+    pipe(iopipes);
+
+    if(fork()) {
+      FILE *iorfh;
+
+      /* Parent */
+      close(iopipes[1]);
+
+      iorfh = fdopen(iopipes[0], "r");
+
+      while(fgets(iorbuf, sizeof(iorbuf), iorfh) && strncmp(iorbuf, "IOR:", 4))
+	/* Just read lines until we get what we're looking for */ ;
+
+      if(strncmp(iorbuf, "IOR:", 4)) {
+	name_service = CORBA_OBJECT_NIL;
+	goto out;
+      }
+
+      fclose(iorfh);
+
+      gdk_property_change(GDK_ROOT_PARENT(), propname, proptype, 8,
+			  GDK_PROP_MODE_REPLACE, iorbuf, strlen(iorbuf));
+      name_service = CORBA_ORB_string_to_object(orb, iorbuf, &ev);
+      
+      if(ev._major != CORBA_NO_EXCEPTION)
+	name_service = CORBA_OBJECT_NIL;
+
+    } else if(fork()) {
+      _exit(0); /* de-zombifier process, so we exit */
+    } else {
+      /* Child of a child. We run the naming service */
+      close(0);
+      close(iopipes[0]);
+      dup2(iopipes[1], 1);
+      dup2(iopipes[1], 2);
+      execlp("orbit-naming-server", "orbit-naming-server", NULL);
+      _exit(1);
+    }
   }
 
-  return name_service;
+ out:
+  retval = CORBA_Object_duplicate(name_service, &ev);
+  CORBA_exception_free(&ev);
+  return retval;
 }
