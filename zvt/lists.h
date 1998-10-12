a5b65c46e91b348ca61331dea6a39403b4f70fe1
Michael Zucci <zucchi@src.gnome.org>
1998-10-12 10:09:15 +0000

    Changed license to LGPL for the 'zvtterm' widget + underlying 	support code.
    svn path=/trunk/; revision=1742

--- zvt/lists.h
+++ zvt/lists.h
@@ -4,18 +4,18 @@
  *  Partly based on list handling code by Marcus Comstedt in libami
  *  from amiwm (an implementation of Amiga Exec-style doubly-linked lists).
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public License
+ *  as published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
