785e6b7afae14519966969770694e01be9f8589e
Owen Taylor <otaylor@gtk.org>
1998-10-18 23:06:22 +0000

    Add in gnome-mime.h, since gnome_uri_list_* is (temporarily?) there.
    Sun Oct 18 02:08:20 1998  Owen Taylor  <otaylor@gtk.org>
    
    	* libgnome/libgnome.h: Add in gnome-mime.h, since
    	gnome_uri_list_* is (temporarily?) there.
    
    	* libgnome/gnome-mime.[ch]: Added functions to decompose
    	data of the type text/uri-list.
    
    Sun Oct 18 18:59:33 1998  Owen Taylor  <otaylor@gtk.org>
    
    	* gnome-mdi.[ch] gnome-color-selector.c gnome-file-entry.c:
    	Adapted to the new GTK+ DND API.
    
    	* gnome-stock.[ch]: Added new function
    	gnome_stock-pixmap_gdk() to get a stock pixmap as
    	a GdkPixmap/GdkMask.
    
    	* gtk-socket.[ch]: Changes to enable DND proxying to
    	the embedded plug.
    
    Sun Oct 18 18:59:11 1998  Owen Taylor  <otaylor@gtk.org>
    
    	* gnome-hello-5-dnd.c: Adapted to work with the
    	new GTK+ DND API.
    
    svn path=/trunk/; revision=1817

--- gnome-hello/gnome-hello-5-dnd.c
+++ gnome-hello/gnome-hello-5-dnd.c
@@ -10,15 +10,30 @@
 #include <config.h>
 #include <gnome.h>
 
-static void hello_cb (GtkWidget *widget, void *data);
 static void quit_cb (GtkWidget *widget, void *data);
-static void drag_cb (GtkWidget *widget, GdkEventDragRequest *event);
-static void drop_highlight_cb(GtkWidget *widget, GdkEvent *event);
-static void drop_cb(GtkWidget *widget, GdkEventDropDataAvailable *event);
+static void drag_data_get_cb (GtkWidget        *widget,
+			      GdkDragContext   *context,
+			      GtkSelectionData *data,
+			      guint             info,
+			      guint             time);
+static void drop_cb          (GtkWidget *widget,
+			      GdkDragContext   *context,
+			      gint              x,
+			      gint              y,
+			      GtkSelectionData *selection_data,
+			      guint             info,
+			      guint             time);
 
 static void prepare_app(void);
 
 GtkWidget *app;
+
+/* An enumeration to identify types of dragged data */
+enum {
+	TARGET_HTML,
+	TARGET_PLAIN,
+	TARGET_URL
+};
 
 int
 main(int argc, char *argv[])
@@ -44,65 +59,54 @@ prepare_app(void)
   GtkWidget *source, *drop_target, *vbox;
 
   /* This is the list of formats the drag source exports */
-  char *drag_types[] = {
-    "text/html",
-    "text/plain"
+  static GtkTargetEntry drag_types[] = {
+	  { "text/html",  0, TARGET_HTML },
+	  { "text/plain", 0, TARGET_PLAIN }
   };
 
-  /* This is the list of formats the drop target suppots */
-  char *drop_types[] = {
-    "text/plain"
+  /* This is the list of formats the drop destination supports */
+  static GtkTargetEntry drop_types[] = {
+	  { "text/plain", 0, TARGET_PLAIN }
   };
 
   /* Make the main window and binds the delete event so you can close
      the program from your WM */
   app = gnome_app_new ("hello", "Hello World Gnomified");
-  gtk_widget_realize (app);
   gtk_signal_connect (GTK_OBJECT (app), "delete_event",
                       GTK_SIGNAL_FUNC (quit_cb),
                       NULL);
 
   vbox = gtk_hbox_new(5, FALSE);
   gnome_app_set_contents ( GNOME_APP (app), vbox);
-  gtk_widget_realize(vbox);
   
   source = gtk_button_new_with_label ("Drag me");
   drop_target = gtk_button_new_with_label ("to this button");
   gtk_container_add(GTK_CONTAINER(vbox), source);
   gtk_container_add(GTK_CONTAINER(vbox), drop_target);
 
-  /* Widgets get realized, as the X-window associated with them
-   * should exist prior to be able to work with DND
-   */
-  gtk_widget_realize(source);
-  gtk_widget_realize(drop_target);
+  gtk_signal_connect (GTK_OBJECT (source), "drag_data_get",
+		      GTK_SIGNAL_FUNC(drag_data_get_cb), NULL);
 
-  /* You currently have to do all the realization before setting
-     up D&D, so once we've done this all is well and we do
-     the D&D-specific stuff */
+  gtk_drag_source_set (source, 
+		       GDK_BUTTON1_MASK,
+		       drag_types, sizeof(drag_types) / sizeof(drag_types[0]),
+		       GDK_ACTION_COPY);
 
-  gtk_signal_connect (GTK_OBJECT (source), "drag_request_event",
-		      GTK_SIGNAL_FUNC(drag_cb), NULL);
-
-  gtk_widget_dnd_drag_set(source, TRUE, drag_types, 2);
   gtk_container_border_width (GTK_CONTAINER (source), 60);
 
-  gtk_signal_connect (GTK_OBJECT (drop_target), "drop_enter_event",
-		      GTK_SIGNAL_FUNC(drop_highlight_cb), NULL);
-  gtk_signal_connect (GTK_OBJECT (drop_target), "drop_leave_event",
-		      GTK_SIGNAL_FUNC(drop_highlight_cb), NULL);
-  gtk_signal_connect (GTK_OBJECT (drop_target), "drop_data_available_event",
+  gtk_signal_connect (GTK_OBJECT (drop_target), "drag_data_received",
 		      GTK_SIGNAL_FUNC(drop_cb), NULL);
-  gtk_widget_dnd_drop_set(drop_target, TRUE, drop_types, 1, FALSE);
+
+  gtk_drag_dest_set (drop_target,
+		     GTK_DEST_DEFAULT_MOTION |
+		     GTK_DEST_DEFAULT_HIGHLIGHT |
+		     GTK_DEST_DEFAULT_DROP,
+		     drop_types, sizeof(drop_types) / sizeof(drop_types[0]),
+		     GDK_ACTION_COPY);
+
   gtk_container_border_width (GTK_CONTAINER (drop_target), 60);
 
-  /* We now show the widgets, the order doesn't matter, but i suggests 
-     showing the main window last so the whole window will popup at
-     once rather than seeing the window pop up, and then the button form
-     inside of it. Although with such simple example, you'd never notice. */
-  gtk_widget_show (source);
-  gtk_widget_show (drop_target);
-  gtk_widget_show (app);
+  gtk_widget_show_all (app);
 }
 
 /* Callbacks functions */
@@ -115,56 +119,53 @@ quit_cb (GtkWidget *widget, void *data)
 }
 
 static void
-drag_cb (GtkWidget *widget, GdkEventDragRequest *event)
+drag_data_get_cb (GtkWidget        *widget,
+		  GdkDragContext   *context,
+		  GtkSelectionData *selection_data,
+		  guint             info,
+		  guint             time)
 {
   char *html_data = "<HTML><HEAD><TITLE>Hi there</TITLE></HEAD><BODY>I am the HTML drag data.</BODY></HTML>";
   char *ascii_data = "Hi there - I am the ASCII drag data.";
   char *url_data = "file1\0file2\0file3";
 
-  g_print("drag_cb\n");
-  if(!strcmp(event->data_type, "text/html"))
-    {
-      gtk_widget_dnd_data_set(widget, (GdkEvent*)event, html_data,
-			      strlen(html_data) + 1);
-    }
-  else if(!strcmp(event->data_type, "text/plain"))
-    {
-      gtk_widget_dnd_data_set(widget, (GdkEvent*)event, ascii_data,
-			      strlen(ascii_data) + 1);
-    } else if (!strcmp (event->data_type, "url:ALL"))
-    {
-      gtk_widget_dnd_data_set (widget, (GdkEvent*)event, url_data, 
-			      strlen(url_data) + 1);
-    }
+  g_print("drag_data_get_cb\n");
+
+  switch (info) {
+  case TARGET_HTML:
+	  gtk_selection_data_set (selection_data,
+				  selection_data->target, 8,
+				  html_data, strlen(html_data));
+	  break;
+  case TARGET_PLAIN:
+	  gtk_selection_data_set (selection_data,
+				  selection_data->target, 8,
+				  ascii_data, strlen(ascii_data));
+	  break;
+  case TARGET_URL:
+	  gtk_selection_data_set (selection_data,
+				  selection_data->target, 8,
+				  url_data, strlen(url_data));
+	  break;
+  }
 }
 
 static void
-drop_highlight_cb(GtkWidget *widget, GdkEvent *event)
-{
-  g_print("drop_highlight_cb\n");
-  if(event->type == GDK_DROP_ENTER)
-    {
-      gtk_widget_set_state(widget, GTK_STATE_PRELIGHT);
-    }
-  else if(event->type == GDK_DROP_LEAVE)
-    {
-      gtk_widget_set_state(widget, GTK_STATE_NORMAL);
-    }
-}
-
-static void
-drop_cb(GtkWidget *widget, GdkEventDropDataAvailable *event)
+drop_cb (GtkWidget *widget,
+	 GdkDragContext   *context,
+	 gint              x,
+	 gint              y,
+	 GtkSelectionData *selection_data,
+	 guint             info,
+	 guint             time)
 {
   g_print("drop_cb\n");
-  if(GTK_IS_BUTTON(widget)
-     && GTK_IS_LABEL(GTK_BUTTON(widget)->child))
-    {
-      if(!strcmp(event->data_type, "text/plain"))
-	{
-	  printf ("Numbytes: %ld\n", event->data_numbytes);
-	  gtk_label_set(GTK_LABEL(GTK_BUTTON(widget)->child), event->data);
-	}
-      /* We could also handle drops of images here by creating
-	 a pixmap inside the button - the sky is the limit */
-    }
+  if (GTK_IS_BUTTON(widget) &&
+      GTK_IS_LABEL(GTK_BUTTON(widget)->child)) {
+	  g_print ("Numbytes: %d\n", selection_data->length);
+	  gtk_label_set(GTK_LABEL(GTK_BUTTON(widget)->child),
+			selection_data->data);
+	  /* We could also handle drops of images here by creating
+	     a pixmap inside the button - the sky is the limit */
+  }
 }
