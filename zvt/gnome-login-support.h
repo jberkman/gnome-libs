86b290f78caea0d1065a1cada4ce93499c3018b7
Michael Zucchi <zucchi@zzedzone.fsi.com.au>
1998-11-09 00:00:52 +0000

    Include pty.h only if it exists, or util.h if that exists (for OpenBSD).
    1998-11-09  Michael Zucchi  <zucchi@zzedzone.fsi.com.au>
    
            * gnome-login-support.h: Include pty.h only if it exists, or
            util.h if that exists (for OpenBSD).
    
    svn path=/trunk/; revision=1941

--- zvt/gnome-login-support.h
+++ zvt/gnome-login-support.h
@@ -2,7 +2,13 @@
 #define _GNOME_LOGIN_SUPPORT_H
 
 #ifdef HAVE_OPENPTY
+# ifdef HAVE_PTY_H
 #    include <pty.h>
+# else
+#    ifdef HAVE_UTIL_H
+#      include <util.h>
+#    endif
+# endif
 #else
 int openpty (int *master_fd, int *slavefd, char *name, struct termios *termp, struct winsize *winp);
 pid_t forkpty (int *master_fd, char *name, struct termios *termp, struct winsize *winp);
