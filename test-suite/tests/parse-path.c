4221ec3c10e6c25c9480e003dc2282f3aeab223b
Raja R Harinath <harinath@src.gnome.org>
1998-12-04 02:54:24 +0000

    New variable -- standin for a #define of the same name in
    * tests/parse-path.c (prefix): New variable -- standin for a
    #define of the same name in `gnome-config.c'.
    (parse-path.cP): Include this file.  It contains the internal
    algorithm we desire to test.
    (test_parse_path): New function.  Moved from `gnome-config.c' and
    renamed from _test_suite_check_parse_path.
    (my_parse_path): Rename from `parse_path' to avoid conflict with
    `parse-path.cP'.
    (run_tests_with_prefix): Simulate `gnome_config_{push,pop}_prefix'.
    (main): Pass a placebo version string to `gnomelib_init'.
    
    * Makefile.am (INCLUDES): Change -I../.. to -I..
    (EXTRA_DISTS): Distribute `parse-path.c' and `parse-path.out'.
    
    svn path=/trunk/; revision=2263

--- test-suite/tests/parse-path.c
+++ test-suite/tests/parse-path.c
@@ -11,63 +11,90 @@
    A non-zero exit code also indicates failure.
 */
 
-#ifndef TEST_INTERNALS
+/* This is a #define in the real code, but this should work for us.  */
+char *prefix;
 
-void
-_test_suite_gnome_config_parse_path (const char *path, gint priv)
+/* Get the definition of parse_path and release_path from here.  */
+#include "libgnome/parse-path.cP"
+
+
+static void test_parse_path (const char *path, gint priv)
 {
-	g_error ("You need to run configure with the --enable-test-internals "
-		 "flag to run this test.");
-}
+	ParsedPath *p = parse_path (path, priv); 
 
+#if 0
+	printf ("%s|%s|%d - %s|%s|%s|%s|%s|%s|\n", (char*) prefix, path, priv,
+		p->file, p->section, p->key, p->def, p->path, p->opath);
 #else
+/* Make 'x' null safe. */
+#define X(x) ((x)?(x):"(null)")
 
-extern void _test_suite_gnome_config_parse_path (const char *, gint);
+	printf ("parse_path (%d):\n\n", priv);
+	printf ("   Prefix:\t%s\n", X(prefix));
+	printf ("   Path:\t%s\n\n", X(path));
+	printf ("   File:\t%s\n", X(p->file));
+	printf ("   Section:\t%s\n", X(p->section));
+	printf ("   Key:\t\t%s\n", X(p->key));
+	printf ("   Def:\t\t%s\n", X(p->def));
+	printf ("   Path:\t%s\n", X(p->path));
+	printf ("   OPath:\t%s\n\n", X(p->opath));
+
+#undef X
 
 #endif
+	fflush (stdout);
+
+	release_path (p);
+}
 
 static void
-parse_path (const char *path)
+my_parse_path (const char *path)
 {
-	_test_suite_gnome_config_parse_path (path, 0);
-	_test_suite_gnome_config_parse_path (path, 1);
+	test_parse_path (path, 0);
+	test_parse_path (path, 1);
 }
 
 static void
 run_tests (void)
 {
-	parse_path ("section/key");
-	parse_path ("section/key=default");
+	my_parse_path ("section/key");
+	my_parse_path ("section/key=default");
 
-	parse_path ("/section/key");
-	parse_path ("/section/key=default");
+	my_parse_path ("/section/key");
+	my_parse_path ("/section/key=default");
 
-	parse_path ("file/section/key");
-	parse_path ("file/section/key=default");
+	my_parse_path ("file/section/key");
+	my_parse_path ("file/section/key=default");
 
-	parse_path ("/file/section/key");
-	parse_path ("/file/section/key=default");
+	my_parse_path ("/file/section/key");
+	my_parse_path ("/file/section/key=default");
 
-	parse_path ("=/file=/section/key");
-	parse_path ("=/file=/section/key=default");
+	my_parse_path ("=/file=/section/key");
+	my_parse_path ("=/file=/section/key=default");
 
-	parse_path ("=/file/section/key");
-	parse_path ("=/file/section/key=default");
+	my_parse_path ("=/file/section/key");
+	my_parse_path ("=/file/section/key=default");
 
 }
 
 static void
-run_tests_with_prefix (const char *prefix)
+run_tests_with_prefix (const char *pfx)
 {
-	gnome_config_push_prefix (prefix);
+        char *old_prefix = prefix;
+
+	/*gnome_config_push_prefix (prefix);*/
+        prefix = pfx;
+
 	run_tests ();
-	gnome_config_pop_prefix ();
+
+	/*gnome_config_pop_prefix ();*/
+	prefix = old_prefix;
 }
 
 int
 main (int argc, char *argv[])
 {
-	gnomelib_init("parse-path");
+	gnomelib_init("parse-path", "0.0");
 
 	gnome_user_dir = "/tmp/.gnome";
 	gnome_user_private_dir = "/tmp/.gnome_private";
--- test-suite/tests/split.c
+++ test-suite/tests/split.c
@@ -9,18 +9,19 @@
    A non-zero exit code also indicates failure.
 */
 
+/* This should probably be moved to glib/testglib.c.  */
 int main(int argc, char *argv[])
 {
 	char **output;
 	int i;
 	gnomelib_init("split", "0.0");
-	output = g_strsplit("foo:bar:baz", ":", -1);
+	output = g_strsplit("foo:bar:baz:qaz", ":", -1);
 	for(i = 0; output[i]; i++) {
 		g_print("%s ", output[i]);
 		g_free(output[i]);
 	}
 	g_print("\n"); g_free(output);
-	output = g_strsplit("foo:bar:baz", ":", 2);
+	output = g_strsplit("foo:bar:baz:qaz", ":", 2);
 	for(i = 0; output[i]; i++) {
 		g_print("%s ", output[i]);
 		g_free(output[i]);
