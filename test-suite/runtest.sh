d632129e80a6757e0e0220d5c1e7512719a53683
Raja R Harinath <harinath@src.gnome.org>
1999-01-18 04:02:29 +0000

    gnome-libs/: Emit test-suite/tests/Makefile.
    gnome-libs/:
    * configure.in (AC_OUTPUT): Emit test-suite/tests/Makefile.
    
    gnome-libs/test-suite/:
    	Overhaul test-suite framework.  Now, you can run individual tests
    	with "make TESTS=foo.test check", like the god of automake meant
    	it to be.
    	* Makefile.am (SUBDIRS): Add `tests/'.
    	(TESTS): List individual tests.
    	(EXTRA_DIST): Don't lists tests/*.
    	(dummy.c): Remove.
    
    	* runtest.sh: Simplify drastically.  Now, is not a full-fledged
    	shell-script, but a shell-script fragment.  It should be sourced
    	with $the_test set to the name of the desired test.
    
    	* tests/Makefile.am: New file.
    	(noinst_PROGRAMS): List names of individual tests.
    	(INCLUDES): Move here from Makefile.am.
    	(LDADD): Likewise.
    
    svn path=/trunk/; revision=2882

--- test-suite/runtest.sh
+++ test-suite/runtest.sh
@@ -1,70 +1,22 @@
-#!/bin/sh
-
-test -z "$srcdir" && srcdir=`dirname $0` && test -z "$srcdir" && srcdir=.
-
-: ${COMPILE:="gcc -g -O2 -I.. -I$srcdir/.. `gtk-config --cflags` -c"}
-: ${LINK:="gcc -g -O2 $LDFLAGS"}
-: ${LDADD:="../libgnomeui/.libs/libgnomeui.a ../libgnome/.libs/libgnome.a `gtk-config --libs`"}
-
-# $LINK passed in sometimes has a `-o check-TESTS'
-LINK=`echo $LINK | sed -e 's,-o check-TESTS,,g'`
-
-exec 3>./results.summary
-if test -z "$*"; then
-	TESTS="`find $srcdir/tests -name '*.c'`"
-else
-	TESTS="$@"
-fi
-
-mkdir results > /dev/null 2>&1 || true
+# this is a -*- sh -*- script fragment
+# source this file with $srcdir and $the_test set.
+# $the_test is the name of the desired test
 
 export srcdir
 
-for I in $TESTS; do
-    TEST=`basename $I .c`
-    if $COMPILE -c $srcdir/tests/$TEST.c -o $TEST.o > /tmp/.$$ 2>&1
-    then : 
-    else
-	echo "CFAIL: $TEST" >&3
-	echo "CFAIL: $TEST"
-	cp /tmp/.$$ results/$TEST.out && rm -f /tmp/.$$
-	failed=yes
-	continue
-    fi
+mkdir results > /dev/null 2>&1 || true
 
-    if $LINK $TEST.o $LDADD $LIBS -o test_$TEST >> /tmp/.$$ 2>&1
-    then :
-    else
-	echo "CFAIL: $TEST" >&3
-	echo "CFAIL: $TEST"
-	cp /tmp/.$$ results/$TEST.out && rm -f /tmp/.$$
-	failed=yes
-	continue
-    fi
+./tests/$the_test > results/$the_test.out 2>&1
+T=$?
+echo "Exit code $T" >> results/$the_test.out
 
-    rm -f /tmp/.$$ $TEST.out
+# magic exit code 77 (EX_NOPERM: permission denied)
+if test $T = 77; then exit 77; fi
 
-    ./test_$TEST > results/$TEST.out 2>&1
-    T=$?
-    echo "Exit code $T" >> results/$TEST.out
-    if test $T != 0; then
-	echo "FAIL: $TEST" >&3
-	echo "FAIL: $TEST"
-	rm -f core
-	failed=yes
-	continue
-    fi
+if test $T = 0; then
+  if diff -q $srcdir/expected/$the_test.out results/$the_test.out; then
+     exit 0
+  fi
+fi
 
-    if diff -q $srcdir/expected/$TEST.out results/$TEST.out; then
-	echo "PASS: $TEST" >&3
-	echo "PASS: $TEST"
-	rm -f $TEST.o test_$TEST
-    else
-	echo "FAIL: $TEST" >&3
-	echo "FAIL: $TEST"
-	failed=yes
-    fi
-done
-
-test "yes" = "$failed" && exit 1
-exit 0
+exit 1
