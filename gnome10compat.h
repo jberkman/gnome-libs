f67fdd6d1c5064cf4933afb6172a50f2b164685a
Elliot Lee <sopwith@src.gnome.org>
1999-11-16 20:14:45 +0000

    gnome10compat.h: Fix include paths.
    gnome10compat.h: Fix include paths.
    
    svn path=/trunk/; revision=4833

--- gnome10compat.h
+++ gnome10compat.h
@@ -1,7 +1,7 @@
 #ifndef GNOME10COMPAT_H
 #define GNOME10COMPAT_H
 
-#include "libgnome/compat/libgnome10compat.h"
-#include "libgnomeui/compat/libgnomeui10compat.h"
+#include <libgnome/compat10/libgnome10compat.h>
+#include <libgnomeui/compat10/libgnomeui10compat.h>
 
 #endif
