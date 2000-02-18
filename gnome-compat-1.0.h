dc6a166643e8ecc69627428a2b98ea2d2487deef
Elliot Lee <sopwith@src.gnome.org>
2000-02-18 18:30:23 +0000

    Check for newer popt, and include it in the needed libs. Give correct
    * configure.in: Check for newer popt, and include it in the needed libs.
    * gnome-compat-1.0.h: Give correct paths to compat headers.
    * gnome-config.in: Fixes to make help output correct.
    * gnome.m4: Reorg so it doesn't do any path searching itself, just uses gnome-config.
    * libgnome/gnome-popt.h: Include popt.h not popt-gnome.h
    * support/Makefile.am: No popt built-in.
    
    svn path=/trunk/; revision=5129

--- gnome-compat-1.0.h
+++ gnome-compat-1.0.h
@@ -1,7 +1,7 @@
 #ifndef GNOME10COMPAT_H
 #define GNOME10COMPAT_H
 
-#include <libgnome/compat10/libgnome10compat.h>
-#include <libgnomeui/compat10/libgnomeui10compat.h>
+#include <compat/1.0/libgnome/libgnome-compat-1.0.h>
+#include <compat/1.0/libgnomeui/libgnomeui-compat-1.0.h>
 
 #endif
